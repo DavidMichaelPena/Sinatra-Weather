7af12ba0837c4ecff1ab7a7e7dfdc3a48ff54096
DavidMichaelPena <alexamenos@gmail.com>
2017-09-16 05:06:34 +0000

    city weather data

--- app.rb
+++ app.rb
@@ -1,15 +1,25 @@
 require 'sinatra'
 require 'config_env'
 require 'httparty'
+require_relative 'models/weather.rb'
+require 'uri'
+
 ConfigEnv.init("#{__dir__}/config/env.rb")
 
 set :port, 3000
 
 get '/' do
-  resp = HTTParty.get(
-    "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=#{ENV['API_KEY']}"
-    )
-  data = JSON.parse(resp.body)
-  @temp = data['main']['temp'] * 9 / 5 - 459.67
+  @cities = [
+    "Minneapolis",
+    "London",
+    "New Orleans",
+    "Chicago",
+    "Phoenix",
+    "Jerusalem"
+  ]
+
+  @city = params[:city] || "Minneapolis"
+  weather = Weather.new(@city)
+  @temp = weather.temp
   erb :index
 end
