25d1d43d9d210114209b61f8a6841cc86381efb2
DavidMichaelPena <alexamenos@gmail.com>
2017-09-16 22:36:59 +0000

    adding redis

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require 'config_env'
 require 'httparty'
 require_relative 'models/weather.rb'
 require 'uri'
+require "redis"
 
 ConfigEnv.init("#{__dir__}/config/env.rb")
+REDIS = Redis.new(url: ENV['REDIS_URL'])
 
 set :port, 3000
 
